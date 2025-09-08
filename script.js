b1ac7fca05cd13c994ec4efd4bc68fd2e77f0729
aNTONIOcoisado <antonio.kawalek@escola.pr.gov.br>
2025-09-08 08:12:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -49,24 +49,24 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Jogar Clash Royale com o objetivo de upar de arena",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                afirmacao: "Vai pra casa jogar clash royale,o que acaba não sendo uma boa idéia porque cai contra um deck de PEKKA e Megacavaleiro nivel 15."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Twitar sobre.",
+                afirmacao: "Após chegar em casa, decide que é uma boa idéia posta no twitter!"
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Angelo decide largar o celular depois de um tempo, e percebe que saiu no jornal nacional que alguem cagou no rio e contaminou a grande são paulo, matando cerca de 8 mil pessoas ",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Se arrepender de seus atos.",
+                afirmacao: "Depois de perceber que Wiliam Bonner estava falando da agua que ele tinha contaminado, se arrepende de ter cagado no rio, se entrega às autoridades e é condenado a 8 anos de cadeia."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Se arrepender de seus atos (segunda opção).",
+                afirmacao: "Vê o anuncio do JN sobre 8 mil mortes pela agua contaminada, mas mesmo arrependido pega o Monza de seu pai e migra ilegalmente para o Paraguai. "
             }
         ]
     },
@@ -82,6 +82,37 @@ function mostraPergunta() {
     caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
 
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
 
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
 
 mostraPergunta();
